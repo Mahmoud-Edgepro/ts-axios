0b9f61cc2ad0bd57ac646eef6c7445df81dc5f00
Mahmoud Abdelhameed <mahardan@hotmail.com>
2025-01-15 00:15:53 +0200

    crypto summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,34 @@
-import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-export type Crypto = {
-  ath: number;
-  atl: number;
-  current_price: number;
-  id: string;
-  name: string;
-  symbol: string;
-  high_24h: number;
-  low_24h: number;
-};
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CryptoSummary from "./components/CryptoSummary";
+import { Crypto } from './Types'
 
 function App() {
   const [cryptos, setCryptos] = useState<Crypto[] | null>();
   useEffect(() => {
-    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd';
-    axios.get(url, {
-      headers: {
-        "accept": "application/json",
-        "x-cg-demo-api-key": "CG-xhxmHbt7eWvXizo8RnmP8Xm2"
-      },
-    }).then((response) => {
-      setCryptos(response.data);
-    });
-  }, [])
-
+    const url =
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd";
+    axios
+      .get(url, {
+        headers: {
+          accept: "application/json",
+          "x-cg-demo-api-key": "CG-xhxmHbt7eWvXizo8RnmP8Xm2",
+        },
+      })
+      .then((response) => {
+        setCryptos(response.data);
+      });
+  }, []);
 
   return (
-    <div className="App">{cryptos ?
-      cryptos.map((crypto)=>{
-        return <p>{crypto.name + ' $' + crypto.current_price}</p>
-      }) : null
-    }</div>
+    <div className="App">
+      {cryptos
+        ? cryptos.map((crypto) => {
+          return <CryptoSummary crypto={crypto} />
+          })
+        : null}
+    </div>
   );
 }
 
